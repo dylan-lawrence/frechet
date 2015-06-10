affd3132146e9924deb9d4db194ebfa6a94ae6ab
Dylan Lawrence <dlawre14@slu.edu>
2015-06-10 03:28:12 -0500

    beginning work on frechet.cpp

--- cplusplus/frechet/frechet/frechet.cpp
+++ cplusplus/frechet/frechet/frechet.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include <iostream>
+
+#include <boost/program_options.hpp>
 #include "CGAL\Simple_cartesian.h"
 
 typedef CGAL::Simple_cartesian<double> Kernel;
@@ -11,8 +13,28 @@ typedef Kernel::Segment_2 Segment_2;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Point_2 p(2, 2);
-	std::cout << "p=" << p << std::endl;
-	return 0;
+	//Define argument parsing
+	try {
+		namespace po = boost::program_options; //simple binding
+
+		po::options_description desc("Options");
+		desc.add_options()
+			("help", "Print help options.")
+			("file1", "A file containing a curve.")
+			("file2", "A different file containing a curve.")
+			("outfile", "The file to write output data.")
+			("filetype", "The format of the curve files.")
+			("epsilon", "The epsilon to use for calculation.")
+			("epsilonrange", "A range of epsilon values to test.")
+			("epsilonstep", "The step between epsilons in epsilonrange.")
+			("verbose", "Activate verbose logging.");
+
+	}
+	catch (std::exception &e) {
+		std::cout << "An error occured in argument parsing" << std::endl;
+	}
+
+	std::cout << "Press enter to exit..." << std::endl;
+	std::cin.get();
 }
 
