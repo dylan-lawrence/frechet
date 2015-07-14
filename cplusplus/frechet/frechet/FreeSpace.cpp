fa21484e9ca7cd6759feb242482e08270799fdb8
Dylan Lawrence <dlawre14@slu.edu>
2015-07-14 13:28:45 -0500

    adjustments and debugging, added sqrt to percent calculation

--- cplusplus/frechet/frechet/FreeSpace.cpp
+++ cplusplus/frechet/frechet/FreeSpace.cpp
@@ -10,7 +10,7 @@ double FreeSpace::PercentAlongCurve(Circular_arc_point_2 ap, Point_2 curvep1, Po
 	Circular_arc_point_2 cp1 = Circular_arc_point_2(curvep1);
 	Circular_arc_point_2 cp2 = Circular_arc_point_2(curvep2);
 
-	return CGAL::to_double((cp1.x() - cp2.x())*(cp1.x() - cp2.x()) + (cp1.y() - cp2.y())*(cp1.y() - cp2.y()) / (cp1.x() - ap.x())*(cp1.x() - ap.x()) + (cp1.y() - ap.y())*(cp1.y() - ap.y()));
+	return std::sqrt(CGAL::to_double((cp1.x() - cp2.x())*(cp1.x() - cp2.x()) + (cp1.y() - cp2.y())*(cp1.y() - cp2.y()) / (cp1.x() - ap.x())*(cp1.x() - ap.x()) + (cp1.y() - ap.y())*(cp1.y() - ap.y())));
 }
 
 FreeSpace::FreeSpace(int n1i, int n2i, Point_2* c1, Point_2* c2, CGAL::Gmpq eps)
@@ -54,11 +54,15 @@ Edge* FreeSpace::CalculateEdge(Point_2 p, Point_2 curvep1, Point_2 curvep2) //cu
 	std::vector<IntersectOut> output;
 	typedef CGAL::Dispatch_output_iterator < CGAL::cpp11::tuple<IntersectOut>, CGAL::cpp0x::tuple<std::back_insert_iterator<std::vector<IntersectOut>>>> Dispatcher;
 
+	std::cout << "Intersecting a circle at: " << p << " with radius " << epsilon << " with the segment from: " << curvep1 << " to " << curvep2 << std::endl;
+
 	Dispatcher disp = CGAL::dispatch_output<IntersectOut>(std::back_inserter(output));
 	CGAL::intersection(l, c, disp);
 
 	if (output.size() > 1) //two intersection points
 	{
+		std::cout << "Found two intersections at: " << CGAL::to_double(output[0].first.x()) << CGAL::to_double(output[0].first.y()) << " and " << CGAL::to_double(output[1].first.x()) << CGAL::to_double(output[1].first.y()) << std::endl;
+
 		//Note: need to special case and check if the intersections are beyond the segment
 		double percent1 = PercentAlongCurve(output[0].first, curvep1, curvep2);
 		double percent2 = PercentAlongCurve(output[1].first, curvep1, curvep2);
