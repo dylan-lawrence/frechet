1d351fb7ccf9c9390a9ed759f4aafef744cc8292
Dylan Lawrence <dlawre14@slu.edu>
2015-08-05 23:07:02 -0500

    more work on python version

--- python/freespace.py
+++ python/freespace.py
@@ -1,4 +1,5 @@
 import cs1graphics as cs1
+from math import sqrt
 
 class Point:
 
@@ -44,7 +45,28 @@ class FreeSpace:
 
     def CalculateSquare(self, n1i, n2i):
         if n1i == 0 and n2i == 0:
-            grid[n1i][n2i].left = CalculateEdge(self.c2[n2i], self.c1[n1i], self.c2[n1i+1])
+            self.grid[n1i][n2i].left = self.CalculateEdge(self.c2[n2i], self.c1[n1i], self.c2[n1i+1])
+            self.grid[n1i][n2i].top = self.CalculateEdge(self.c1[n1i+1], self.c2[n2i], self.c2[n2i+1])
+            self.grid[n1i][n2i].right = self.CalculateEdge(self.c2[n2i+1], self.c1[n1i], self.c2[n1i+1])
+            self.grid[n1i][n2i].bottom = self.CalculateEdge(self.c1[n1i], self.c2[n2i], self.c2[n2i+1])
+        elif n1i == 0:
+            self.grid[n1i][n2i].left = self.grid[n1i][n2i-1].right
+
+            self.grid[n1i][n2i].top = self.CalculateEdge(self.c1[n1i+1], self.c2[n2i], self.c2[n2i+1])
+            self.grid[n1i][n2i].right = self.CalculateEdge(self.c2[n2i+1], self.c1[n1i], self.c2[n1i+1])
+            self.grid[n1i][n2i].bottom = self.CalculateEdge(self.c1[n1i], self.c2[n2i], self.c2[n2i+1])
+        elif n2i == 0:
+            self.grid[n1i][n2i].bottom = self.grid[n1i-1][n2i].top
+
+            self.grid[n1i][n2i].left = self.CalculateEdge(self.c2[n2i], self.c1[n1i], self.c2[n1i+1])
+            self.grid[n1i][n2i].top = self.CalculateEdge(self.c1[n1i+1], self.c2[n2i], self.c2[n2i+1])
+            self.grid[n1i][n2i].right = self.CalculateEdge(self.c2[n2i+1], self.c1[n1i], self.c2[n1i+1])
+        else:
+            self.grid[n1i][n2i].left = self.grid[n1i][n2i-1].right
+            self.grid[n1i][n2i].bottom = self.grid[n1i-1][n2i].top
+
+            self.grid[n1i][n2i].top = self.CalculateEdge(self.c1[n1i+1], self.c2[n2i], self.c2[n2i+1])
+            self.grid[n1i][n2i].right = self.CalculateEdge(self.c2[n2i+1], self.c1[n1i], self.c2[n1i+1])
 
     def CalculateEdge(self, p, cp1, cp2):
         edge = Edge()
@@ -59,6 +81,7 @@ class FreeSpace:
         if a == 0:
             edge.start = -1
             edge.end = -1
+            return edge
 
         det = b**2 - 4*a*c
 
@@ -66,11 +89,44 @@ class FreeSpace:
           print('Determinant is less than 0, edge not reachable')
           edge.start = -1
           edge.end = -1
+          return edge
 
         edge.end = (-b + sqrt(det))/(2*a)
         edge.start = (-b - sqrt(det))/(2*a)
 
+        if (edge.end < 0 and edge.start < 0): #both intersections are off segment
+            edge.start = -1
+            edge.end = -1
+        elif (edge.end > 1 and edge.start > 1): #both intersections are off segment
+            edge.start = -1
+            edge.end = -1
+        elif (edge.start == edge.end and (edge.start > 1 or edge.start < 0)): #single intersection not on segment
+            edge.start = -1
+            edge.end = -1
+        else: #need to fix > 1 or < 0
+            if edge.start > 1:
+                edge.start = 1
+            if edge.end > 1:
+                edge.end = 1
+            if edge.start < 0:
+                edge.start = 0
+            if edge.end < 0:
+                edge.end = 0
+
         return edge
+
+class DrawingTool:
+
+    def __init__(self):
+        self.fscanvas = None
+
+    def DrawFreeSpace(self, fs):
+        self.fscanvas = cs1.Canvas(fs.n1 * 50 + 200, fs.n2 * 50 + 200)
+        y = 0
+        for i in range(fs.n1-1):
+            for j in range (fs.n2-1):
+                self.fscanvas.add(cs1.Square())
+            y += 50
 
 
 if __name__ == '__main__':
@@ -114,3 +170,9 @@ if __name__ == '__main__':
             curr = f.readline()
 
     fs = FreeSpace(len(curve1), len(curve2), curve1, curve2, epsilon)
+    for i in range(len(curve1)-1):
+        for j in range(len(curve2)-1):
+            fs.CalculateSquare(i,j)
+
+    draw = DrawingTool()
+    draw.DrawFreeSpace(fs)
